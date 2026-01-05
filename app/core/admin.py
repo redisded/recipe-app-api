02fb97b8c8437da54faa47e8a05a6e3b749a4942
red <red@nlefevre.fr>
2026-01-05 16:00:10 +0100

    Lint

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,6 @@
 Django admin customization for core app.
 """
 
-from os import name
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -23,7 +22,7 @@ class UserAdmin(BaseUserAdmin):
                     'is_active',
                     'is_staff',
                     'is_superuser',
-                    )
+                )
             }
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
@@ -33,15 +32,16 @@ class UserAdmin(BaseUserAdmin):
         (None, {
             'classes': ('wide',),
             'fields': (
-                    'email',
-                    'password1', 
-                    'password2',
-                    'name',
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
-                )
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
